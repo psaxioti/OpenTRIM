d192a3602d8cf9d234dd7b1030fb6441be6315a9
George Apostolopoulos <gapost@ipta.demokritos.gr>
2024-12-12 11:21:36 +0200

    Fix test/octave/plot_benchmark.m

--- test/octave/plot_benchmark.m
+++ test/octave/plot_benchmark.m
@@ -1,68 +1,70 @@
 clear
 
 # Benchmark Number
-nb = 6;
+nb = 7;
 
 # Load HDF5 Results
 pkg load hdf5oct
-ipp = h5load(['../iradina++/b' num2str(nb) '/b' num2str(nb) '.h5']);
+data = h5load(['../ions/b' num2str(nb) '/b' num2str(nb) '.h5']);
 
 # Title
-titlestr = ipp.title;
+titlestr = data.run_info.title;
 
 # x axis = cell centers
-x = ipp.grid.cell_xyz(:,1);
+x = data.target.grid.cell_xyz(:,1);
 
+# atom labels
+atom_labels = data.target.atoms.label;
 
 # Figure 1 = Defects vs. x
 figure 1
 clf
 subplot(2,2,1)
-plot(x,ipp.tally.defects.Implantations(:,1))
+plot(x,data.tally.defects.Implantations(:,1))
 title([titlestr ' - Implanted Ions'])
-legend(ipp.atom.label{1})
+legend(atom_labels{1})
 xlabel('x (nm)')
 subplot(2,2,2)
-plot(x,ipp.tally.defects.Implantations(:,2:end))
+plot(x,data.tally.defects.Implantations(:,2:end))
 title([titlestr ' - I'])
-legend(ipp.atom.label{2:end})
+legend(atom_labels{2:end})
 xlabel('x (nm)')
 subplot(2,2,3)
-plot(x,ipp.tally.defects.Vacancies)
+plot(x,data.tally.defects.Vacancies)
 title([titlestr ' - V'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 subplot(2,2,4)
-plot(x,ipp.tally.defects.Replacements)
+plot(x,data.tally.defects.Replacements)
 title([titlestr ' - R'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 
 # Figure 2 = Energy Deposition vs. x
 figure 2
 clf
 subplot(2,2,1)
-plot(x,ipp.tally.energy_deposition.Ionization)
+plot(x,data.tally.energy_deposition.Ionization)
 title([titlestr ' - Ionization'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 ylabel('eV')
 subplot(2,2,2)
-plot(x,ipp.tally.energy_deposition.Phonons)
+plot(x,data.tally.energy_deposition.Phonons)
 title([titlestr ' - Phonons'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 ylabel('eV')
 subplot(2,2,3)
-plot(x,ipp.tally.energy_deposition.PKA)
+plot(x,data.tally.energy_deposition.PKA)
 title([titlestr ' - Er'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 ylabel('eV')
 subplot(2,2,4)
-plot(x,(ipp.tally.energy_deposition.PKA)./(ipp.tally.defects.PKAs))
+plot(x,(data.tally.energy_deposition.PKA)./(data.tally.defects.PKAs))
 title([titlestr ' - Er/PKA'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 ylabel('eV')
 
@@ -70,20 +72,20 @@ ylabel('eV')
 figure 3
 clf
 lbls = {};
-for i=2:length(ipp.atom.label),
-  lbls = { lbls{:}, [ipp.atom.label{i} ' Tdam']};
+for i=2:length(atom_labels),
+  lbls = { lbls{:}, [atom_labels{i} ' Tdam']};
 end
 lbls = { lbls{:}, 'Total Tdam' };
-for i=2:length(ipp.atom.label),
-  lbls = { lbls{:}, [ipp.atom.label{i} ' Eph']};
+for i=2:length(atom_labels),
+  lbls = { lbls{:}, [atom_labels{i} ' Eph']};
 end
 lbls = { lbls{:}, 'Total Eph' };
 
 subplot(2,2,1)
-plot(x,ipp.tally.damage.Tdam(:,2:end), ...
-  x,sum(ipp.tally.damage.Tdam(:,2:end),2), ...
-  x,ipp.tally.energy_deposition.Phonons(:,2:end),...
-  x,sum(ipp.tally.energy_deposition.Phonons(:,2:end),2))
+plot(x,data.tally.damage.Tdam(:,2:end), ...
+  x,sum(data.tally.damage.Tdam(:,2:end),2), ...
+  x,data.tally.energy_deposition.Phonons(:,2:end),...
+  x,sum(data.tally.energy_deposition.Phonons(:,2:end),2))
 title([titlestr ' - Tdam'])
 legend(lbls)
 xlabel('x (nm)')
@@ -91,51 +93,51 @@ ylabel('eV')
 
 
 lbls = {};
-for i=2:length(ipp.atom.label),
-  lbls = { lbls{:}, [ipp.atom.label{i} ' FC-NRT']};
+for i=2:length(atom_labels),
+  lbls = { lbls{:}, [atom_labels{i} ' FC-NRT']};
 end
 lbls = { lbls{:}, 'Total FC-NRT' };
-for i=2:length(ipp.atom.label),
-  lbls = { lbls{:}, [ipp.atom.label{i} ' FC']};
+for i=2:length(atom_labels),
+  lbls = { lbls{:}, [atom_labels{i} ' FC']};
 end
 lbls = { lbls{:}, 'Total FC' };
 
 subplot(2,2,2)
-plot(x,ipp.tally.damage.Vnrt(:,2:end), ...
-  x,sum(ipp.tally.damage.Vnrt(:,2:end),2), ...
-  x,ipp.tally.defects.Vacancies(:,2:end),...
-  x,sum(ipp.tally.defects.Vacancies(:,2:end),2))
+plot(x,data.tally.damage.Vnrt(:,2:end), ...
+  x,sum(data.tally.damage.Vnrt(:,2:end),2), ...
+  x,data.tally.defects.Vacancies(:,2:end),...
+  x,sum(data.tally.defects.Vacancies(:,2:end),2))
 title([titlestr ' - Vac'])
 legend(lbls)
 xlabel('x (nm)')
 
 
 lbls = {};
-for i=2:length(ipp.atom.label),
-  lbls = { lbls{:}, [ipp.atom.label{i} ' Tdam']};
+for i=2:length(atom_labels),
+  lbls = { lbls{:}, [atom_labels{i} ' Tdam']};
 end
 lbls = { lbls{:}, 'Total Tdam', 'Total Eph' };
-pka = ipp.tally.defects.PKAs(:,2:end);
+pka = data.tally.defects.PKAs(:,2:end);
 
 subplot(2,2,3)
-plot(x,ipp.tally.damage.Tdam(:,2:end)./pka, ...
-  x,sum(ipp.tally.damage.Tdam(:,2:end),2)./sum(pka,2), ...
-  x,sum(ipp.tally.energy_deposition.Phonons(:,2:end),2)./sum(pka,2))
+plot(x,data.tally.damage.Tdam(:,2:end)./pka, ...
+  x,sum(data.tally.damage.Tdam(:,2:end),2)./sum(pka,2), ...
+  x,sum(data.tally.energy_deposition.Phonons(:,2:end),2)./sum(pka,2))
 title([titlestr ' - Tdam/PKA'])
 legend(lbls)
 xlabel('x (nm)')
 ylabel('eV')
 
 lbls = {};
-for i=2:length(ipp.atom.label),
-  lbls = { lbls{:}, [ipp.atom.label{i} ' FC-NRT']};
+for i=2:length(atom_labels),
+  lbls = { lbls{:}, [atom_labels{i} ' FC-NRT']};
 end
 lbls = { lbls{:}, 'Total FC-NRT', 'Total FC' };
 
 subplot(2,2,4)
-plot(x,ipp.tally.damage.Vnrt(:,2:end)./pka,...
-     x,sum(ipp.tally.damage.Vnrt(:,2:end),2)./sum(pka,2),...
-     x,sum(ipp.tally.defects.Vacancies(:,2:end),2)./sum(pka,2))
+plot(x,data.tally.damage.Vnrt(:,2:end)./pka,...
+     x,sum(data.tally.damage.Vnrt(:,2:end),2)./sum(pka,2),...
+     x,sum(data.tally.defects.Vacancies(:,2:end),2)./sum(pka,2))
 title([titlestr ' - Vac/PKA'])
 legend(lbls)
 xlabel('x (nm)')
@@ -144,51 +146,54 @@ xlabel('x (nm)')
 figure 4
 clf
 subplot(2,2,1)
-plot(x,ipp.tally.ion_stat.collisions)
+plot(x,data.tally.ion_stat.collisions)
 title([titlestr ' - Collisions'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 subplot(2,2,2)
-plot(x,ipp.tally.defects.PKAs)
+plot(x,data.tally.defects.PKAs)
 title([titlestr ' - PKAs'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 
 subplot(2,2,3)
-plot(x,ipp.tally.ion_stat.flight_path./ipp.tally.ion_stat.collisions)
+plot(x,data.tally.ion_stat.flight_path./data.tally.ion_stat.collisions)
 title([titlestr ' - mfp'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 ylabel('nm')
 subplot(2,2,4)
-plot(x,ipp.tally.defects.Lost)
+plot(x,data.tally.defects.Lost)
 title([titlestr ' - Lost ions'])
-legend(ipp.atom.label)
+legend(atom_labels)
 xlabel('x (nm)')
 
 # PDF report name
-pname = ['../iradina++/b' num2str(nb) '/b' num2str(nb) '.pdf'];
+pname = ['../ions/b' num2str(nb) '/b' num2str(nb) '.pdf'];
 
+## TODO: fix this code for v0.3.3
 % Get the table in string form.
-TString = sprintf('{\\bf\\fontsize{20}%s}\n\n',ipp.title);
-TString = [TString evalc(['table_compare_all(' num2str(nb) ')'])];
+# TString = sprintf('{\\bf\\fontsize{20}%s}\n\n',titlestr);
+# TString = [TString evalc(['table_compare_all(' num2str(nb) ')'])];
 
 
-% Get a fixed-width font.
-FixedWidth = get(0,'FixedWidthFontName');
+# % Get a fixed-width font.
+# FixedWidth = get(0,'FixedWidthFontName');
 
-figure 5
-clf
+# figure 5
+# clf
+# set(gcf,'papertype','a4','paperorientation','landscape')
+# % Output the table using the annotation command.
+# annotation('textbox',[0.3 0 1 1],'string',TString,...
+#     'FontName',FixedWidth,'Units','Normalized',...
+#     "verticalalignment",'middle',...
+#     'fitboxtotext','off','edgecolor','none');
+
+figure 1
 set(gcf,'papertype','a4','paperorientation','landscape')
-% Output the table using the annotation command.
-annotation('textbox',[0.3 0 1 1],'string',TString,...
-    'FontName',FixedWidth,'Units','Normalized',...
-    "verticalalignment",'middle',...
-    'fitboxtotext','off','edgecolor','none');
-
 print(gcf,'-dpdf','-fillpage',pname)
 
-for i=1:4
+for i=2:4
   figure(i)
   set(gcf,'papertype','a4','paperorientation','landscape')
   print(gcf,'-dpdf','-fillpage','-append',pname)
