f5851cbbe19aa2a01c21c7737f5151cbf2b38462
Michail Axiotis <psaxioti@gmail.com>
2025-02-13 15:11:37 +0200

    FetchContent first implementation
     - Removed local copy of external programs from extern folder
     - Added Externals.cmake with the definition of the external projects
     - Added HelperFunctions.cmake
     - Try to clean up CMakeLists

---
+++ cmake/Externals.cmake
@@ -0,0 +1,70 @@
+include(FetchContent)
+
+set(FETCHCONTENT_BASE_DIR ${CMAKE_BINARY_DIR}/external)
+set(FETCHCONTENT_QUIET FALSE)
+set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
+
+FetchContent_Declare(external_cxxopts
+   GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
+   GIT_TAG v3.1.1
+   GIT_SUBMODULES_RECURSE FALSE
+   GIT_SHALLOW TRUE
+   EXCLUDE_FROM_ALL
+   SOURCE_SUBDIR dummy
+)
+
+FetchContent_Declare(external_highfive
+   GIT_REPOSITORY git@github.com:BlueBrain/HighFive.git
+   GIT_TAG v2.9.0
+   GIT_SUBMODULES_RECURSE FALSE
+   GIT_SHALLOW TRUE
+   EXCLUDE_FROM_ALL
+   SOURCE_SUBDIR dummy
+)
+
+FetchContent_Declare(external_isotope
+   GIT_REPOSITORY git@github.com:Gregstrq/Isotope-data.git
+   GIT_TAG main
+   GIT_SUBMODULES_RECURSE FALSE
+   GIT_SHALLOW TRUE
+   EXCLUDE_FROM_ALL
+   SOURCE_SUBDIR dummy
+)
+
+FetchContent_Declare(external_json
+   GIT_REPOSITORY git@github.com:nlohmann/json.git
+   GIT_TAG v3.11.3
+   GIT_SUBMODULES_RECURSE FALSE
+   GIT_SHALLOW TRUE
+   EXCLUDE_FROM_ALL
+   SOURCE_SUBDIR dummy
+)
+
+FetchContent_Declare(external_periodic
+   GIT_REPOSITORY git@github.com:Bowserinator/Periodic-Table-JSON.git
+   GIT_TAG v.4.0.0
+   GIT_SUBMODULES_RECURSE FALSE
+   GIT_SHALLOW TRUE
+   EXCLUDE_FROM_ALL
+   SOURCE_SUBDIR dummy
+)
+
+# FetchContent_Declare(external_progress
+#    GIT_REPOSITORY git@github.com:gipert/progressbar.git
+#    GIT_TAG v2.1
+#    GIT_SUBMODULES_RECURSE FALSE
+#    GIT_SHALLOW TRUE
+#    EXCLUDE_FROM_ALL
+#    SOURCE_SUBDIR dummy
+# )
+
+if (BUILD_GUI)
+   FetchContent_Declare(external_qmatplotwidget
+      GIT_REPOSITORY https://gitlab.com/qdaq/qmatplotwidget.git
+      GIT_TAG master
+      GIT_SUBMODULES_RECURSE FALSE
+      GIT_SHALLOW TRUE
+      EXCLUDE_FROM_ALL
+      SOURCE_SUBDIR dummy
+   )
+endif()
---
+++ cmake/HelperFunctions.cmake
@@ -0,0 +1,38 @@
+# add_executable(gendedx
+#     src/gendedx.cpp
+#     src/periodic_table.h
+#     ${PTABLE_DATA_CPP})
+
+add_executable(genptable EXCLUDE_FROM_ALL
+    src/genptable.cpp
+)
+
+FetchContent_MakeAvailable(external_isotope)
+FetchContent_MakeAvailable(external_periodic)
+
+set(ISOTOPE_CSV ${external_isotope_SOURCE_DIR}/isotopes_data.csv)
+set(PTABLE_CSV ${external_periodic_SOURCE_DIR}/PeriodicTableCSV.csv)
+set(PTABLE_DATA_CPP periodic_table.cpp)
+
+add_custom_command(OUTPUT ${PTABLE_DATA_CPP}
+    COMMAND genptable ${ISOTOPE_CSV} ${PTABLE_CSV} ${PTABLE_DATA_CPP}
+    DEPENDS genptable
+)
+
+add_executable(gencorteo EXCLUDE_FROM_ALL
+    src/gencorteo.cpp
+)
+
+FetchContent_MakeAvailable(external_cxxopts)
+
+target_include_directories(gencorteo PRIVATE
+    ${external_cxxopts_SOURCE_DIR}/include)
+
+function(add_dedx_library screeningName)
+    add_custom_command(OUTPUT xs_${screeningName}_data.cpp
+        COMMAND gencorteo -s ${screeningName} DEPENDS gencorteo
+    )
+    add_library(xs_${screeningName} SHARED
+        ${CMAKE_CURRENT_BINARY_DIR}/xs_${screeningName}_data.cpp
+    )
+endfunction()
