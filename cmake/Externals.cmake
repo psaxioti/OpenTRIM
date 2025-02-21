437f497b22e0e094e7d0fa577d7a22854c7c3ed9
Michail Axiotis <psaxioti@gmail.com>
2025-02-21 16:32:04 +0200

    Changes to cmake files
     - Changed CMakeLists to compile with cmave versions prior to 3.23
     - Added option to Externals to compile for package building when no internet is present and externals are downloaded localy

--- cmake/Externals.cmake
+++ cmake/Externals.cmake
@@ -4,6 +4,8 @@ set(FETCHCONTENT_BASE_DIR ${CMAKE_BINARY_DIR}/external)
 set(FETCHCONTENT_QUIET FALSE)
 set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
 
+option(PACKAGE_BUILD "Flag for building binary package without internet and localy downloaded externals." OFF)
+
 FetchContent_Declare(external_cxxopts
    GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
    GIT_TAG v3.1.1
@@ -12,7 +14,11 @@ FetchContent_Declare(external_cxxopts
    EXCLUDE_FROM_ALL
    SOURCE_SUBDIR dummy
 )
-FetchContent_MakeAvailable(external_cxxopts)
+if(PACKAGE_BUILD)
+   set(external_cxxopts_SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/ext1)
+else()
+   FetchContent_MakeAvailable(external_cxxopts)
+endif()
 
 FetchContent_Declare(external_highfive
    GIT_REPOSITORY https://github.com/BlueBrain/HighFive.git
@@ -22,7 +28,11 @@ FetchContent_Declare(external_highfive
    EXCLUDE_FROM_ALL
    SOURCE_SUBDIR dummy
 )
-FetchContent_MakeAvailable(external_highfive)
+if(PACKAGE_BUILD)
+   set(external_highfive_SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/ext2)
+else()
+   FetchContent_MakeAvailable(external_highfive)
+endif()
 
 FetchContent_Declare(external_isotope
    GIT_REPOSITORY https://github.com/Gregstrq/Isotope-data.git
@@ -32,7 +42,11 @@ FetchContent_Declare(external_isotope
    EXCLUDE_FROM_ALL
    SOURCE_SUBDIR dummy
 )
-FetchContent_MakeAvailable(external_isotope)
+if(PACKAGE_BUILD)
+   set(external_isotope_SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/ext3)
+else()
+   FetchContent_MakeAvailable(external_isotope)
+endif()
 
 FetchContent_Declare(external_json
    GIT_REPOSITORY https://github.com/nlohmann/json.git
@@ -42,7 +56,11 @@ FetchContent_Declare(external_json
    EXCLUDE_FROM_ALL
    SOURCE_SUBDIR dummy
 )
-FetchContent_MakeAvailable(external_json)
+if(PACKAGE_BUILD)
+   set(external_json_SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/ext4)
+else()
+   FetchContent_MakeAvailable(external_json)
+endif()
 
 FetchContent_Declare(external_periodic
    GIT_REPOSITORY https://github.com/Bowserinator/Periodic-Table-JSON.git
@@ -52,7 +70,11 @@ FetchContent_Declare(external_periodic
    EXCLUDE_FROM_ALL
    SOURCE_SUBDIR dummy
 )
-FetchContent_MakeAvailable(external_periodic)
+if(PACKAGE_BUILD)
+   set(external_periodic_SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/ext5)
+else()
+   FetchContent_MakeAvailable(external_periodic)
+endif()
 
 if (BUILD_GUI)
    FetchContent_Declare(external_qmatplotwidget
@@ -63,5 +85,9 @@ if (BUILD_GUI)
       EXCLUDE_FROM_ALL
       SOURCE_SUBDIR dummy
    )
-   FetchContent_MakeAvailable(external_qmatplotwidget)
+   if(PACKAGE_BUILD)
+      set(external_qmatplotwidget_SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/ext6)
+   else()
+         FetchContent_MakeAvailable(external_qmatplotwidget)
+   endif()
 endif()
